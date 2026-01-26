4fe30c5ddca1b2564725cf2bd80d41a927354501
beyondorigins <dima.a.ivlev@gmail.com>
2026-01-26 14:33:55 +0300

    Initial Commit

---
+++ reversi/reversiApp.swift
@@ -0,0 +1,17 @@
+//
+//  reversiApp.swift
+//  reversi
+//
+//  Created by beyondorigins on 26.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct reversiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
